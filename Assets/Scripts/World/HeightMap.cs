33087743abe8fac3b891c02ff513a807f0da03f2
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-02-05 03:07:50 -0500

    Added IndexToHeight function

--- Assets/Scripts/World/HeightMap.cs
+++ Assets/Scripts/World/HeightMap.cs
@@ -30,10 +30,78 @@ public class HeightMap
         return IndexToHeight(row, column);
     }
 
+    // This treats the heightmap as squares
     public float IndexToHeight(float row, float column)
     {
-        int rowIndex = Mathf.RoundToInt(row);
-        int columnIndex = Mathf.RoundToInt(column);
-        return heightMap[rowIndex, columnIndex];
+        int topIndex = Mathf.FloorToInt(row);
+        int leftIndex = Mathf.FloorToInt(column);
+        int bottomIndex = Mathf.CeilToInt(row);
+        int rightIndex = Mathf.CeilToInt(column);
+
+        float topLeftHeight = heightMap[topIndex, leftIndex];
+        float topRightHeight = heightMap[topIndex, rightIndex];
+        float bottomRightHeight = heightMap[bottomIndex, rightIndex];
+        float bottomLeftHeight = heightMap[bottomIndex, leftIndex];
+
+        float horizontalT = column - leftIndex;
+        float verticalT = row - topIndex;
+
+        float topHeight = Mathf.Lerp(topLeftHeight, topRightHeight, horizontalT);
+        float bottomHeight = Mathf.Lerp(bottomLeftHeight, bottomRightHeight, horizontalT);
+    
+        return Mathf.Lerp(topHeight, bottomHeight, verticalT);
+    }
+
+    // This is a backup that does all the plane math manually
+    //public float IndexToHeightPrecise(float row, float column)
+    //{
+    //    // DON'T GET CONFUSED, ROW GOES DOWN. SO BOTTOM_INDEX > TOP_INDEX
+    //    int topIndex = Mathf.FloorToInt(row);
+    //    int leftIndex = Mathf.FloorToInt(column);
+    //    int bottomIndex = Mathf.CeilToInt(row);
+    //    int rightIndex = Mathf.CeilToInt(column);
+
+    //    float topLeftHeight = heightMap[topIndex, leftIndex];
+    //    float topRightHeight = heightMap[topIndex, rightIndex];
+    //    float bottomRightHeight = heightMap[bottomIndex, rightIndex];
+    //    float bottomLeftHeight = heightMap[bottomIndex, leftIndex];
+   
+    //    Vector3 topLeft = new Vector3(leftIndex, topLeftHeight, topIndex);
+    //    Vector3 bottomRight = new Vector3(rightIndex, bottomRightHeight, bottomIndex);
+    //    Vector3 otherPoint = row - topIndex > column - leftIndex // Test which triangle the index is in
+    //        ? new Vector3(rightIndex, bottomLeftHeight, topIndex)
+    //        : new Vector3(leftIndex, topRightHeight, bottomIndex);
+
+    //    Vector3 vec1 = topLeft - otherPoint;
+    //    Vector3 vec2 = bottomRight - otherPoint;
+    //    Vector3 normal = Vector3.Cross(vec1, vec2);
+
+    //    float d = Vector3.Dot(normal, topLeft); // d when the plane is in the form ax + by + cz = d
+    //    return (d - normal.x * column - normal.z * row) / normal.y;
+    //}
+
+    // This treats the heightmap as triangles
+    public float IndexToHeightPrecise(float row, float column)
+    {
+        // DON'T GET CONFUSED, ROW GOES DOWN. SO BOTTOM_INDEX > TOP_INDEX
+        int topIndex = Mathf.FloorToInt(row);
+        int leftIndex = Mathf.FloorToInt(column);
+        int bottomIndex = Mathf.CeilToInt(row);
+        int rightIndex = Mathf.CeilToInt(column);
+
+        float topLeftHeight = heightMap[topIndex, leftIndex];
+        float topRightHeight = heightMap[topIndex, rightIndex];
+        float bottomRightHeight = heightMap[bottomIndex, rightIndex];
+        float bottomLeftHeight = heightMap[bottomIndex, leftIndex];
+   
+        Vector3 topLeft = new Vector3(leftIndex, topLeftHeight, topIndex);
+        Vector3 bottomRight = new Vector3(rightIndex, bottomRightHeight, bottomIndex);
+        Vector3 otherPoint = row - topIndex > column - leftIndex // Test which triangle the index is in
+            ? new Vector3(leftIndex, bottomLeftHeight, bottomIndex)
+            : new Vector3(rightIndex, topRightHeight, topIndex);
+
+        Plane plane = new Plane(topLeft, bottomRight, otherPoint);
+        plane.Raycast(new Ray(new Vector3(column, 0, row), Vector3.up), out float distance);
+        return distance;
     }
 }
