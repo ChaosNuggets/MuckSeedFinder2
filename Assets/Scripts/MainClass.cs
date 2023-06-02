549a4c8cbe17c264a3003a513d85e037f3e63cf1
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-06-01 22:20:46 -0400

    got time remaining and speed to work

--- Assets/Scripts/MainClass.cs
+++ Assets/Scripts/MainClass.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class MainClass : MonoBehaviour
 {
-    public TextMeshProUGUI seedsFoundText;
+    public TextMeshProUGUI seedsTestedText;
+    public TextMeshProUGUI estimatedTimeText;
+    public TextMeshProUGUI speedText;
 
     private readonly SeedCalculator seedCalculator = new(int.MinValue);
     private int startSeed;
 
-    private void Update()
+    private void FindSeeds()
     {
         const float MAX_DISTANCE_TO_LOG = 3000;
-        const int SEEDS_BETWEEN_UPDATES = 10000;
-
-        seedsFoundText.text = $"Seeds Tested:\n{startSeed - int.MinValue} / {uint.MaxValue}";
+        const int SEEDS_BETWEEN_UPDATES = 50000;
 
         int endSeed = startSeed + SEEDS_BETWEEN_UPDATES;
         if (endSeed < startSeed) // If statement for handling overflow
@@ -41,6 +41,30 @@ public class MainClass : MonoBehaviour
         }
 
         startSeed = seed;
+    }
+
+    private void UpdateText()
+    {
+        const int MINUTES_PER_HOUR = 60;
+        const int SECONDS_PER_MINUTE = 60;
+
+        int numTestedSeeds = startSeed - 1 - int.MinValue;
+        seedsTestedText.text = $"Seeds Tested:\n{numTestedSeeds} / {uint.MaxValue}";
+
+        float secondsLeft = Time.unscaledTime / numTestedSeeds * (uint.MaxValue - numTestedSeeds);
+        int minutesLeft = Mathf.RoundToInt(secondsLeft / SECONDS_PER_MINUTE);
+        int hoursLeft = minutesLeft / MINUTES_PER_HOUR;
+        minutesLeft %= MINUTES_PER_HOUR;
+
+        estimatedTimeText.text = $"Estimated Time Remaining:\n{hoursLeft} hr {minutesLeft} min";
+
+        speedText.text = $"Speed:\n{Mathf.RoundToInt(numTestedSeeds / Time.unscaledTime)} seeds / sec";
+    }
+
+    private void Update()
+    {
+        UpdateText();
+        FindSeeds();
     }
 
     private void Awake()
