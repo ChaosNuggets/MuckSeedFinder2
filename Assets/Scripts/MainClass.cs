804ce3b798b9b4f94f25ad061f2860c1d35a78b7
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-06-04 02:20:04 -0400

    got multithreading kind of working besides a skip over seed bug

--- Assets/Scripts/MainClass.cs
+++ Assets/Scripts/MainClass.cs
@@ -10,62 +10,124 @@ public class MainClass : MonoBehaviour
     public TextMeshProUGUI estimatedTimeText;
     public TextMeshProUGUI speedText;
 
-    private const int NUM_THREADS = 2;
+    private const int NUM_THREADS = 5;
+    private const int NUM_SEEDS_PER_FRAME = 10;
     private readonly int[] startSeeds = new int[NUM_THREADS];
-    private readonly int[] currentSeeds = new int[NUM_THREADS];
     private readonly int[] endSeeds = new int[NUM_THREADS];
 
     private readonly SeedCalculator[] seedCalculators = new SeedCalculator[NUM_THREADS];
 
-    private static List<(int, float)> FindSeeds(SeedCalculator seedCalculator, int startSeed, int endSeed, out int currentSeed)
+    private static (int[], HeightMap[]) GetHeightMaps(SeedCalculator seedCalculator, int startSeed, int endSeed)
+    {
+        HeightMap[] heightMaps = new HeightMap[NUM_SEEDS_PER_FRAME];
+        int[] seeds = seedCalculator.CalculateNextGodSeeds(NUM_SEEDS_PER_FRAME);
+
+        // seeds[i] >= startSeed is for handling the overflow edge case
+        int i = 0;
+        for (; i < NUM_SEEDS_PER_FRAME && seeds[i] <= endSeed && seeds[i] >= startSeed; i++)
+        {
+            heightMaps[i] = new HeightMap(seeds[i]);
+        }
+
+        // For the seeds that are out of the bounds of startSeed and endSeed, set the heightMap to null
+        for (; i < NUM_SEEDS_PER_FRAME; i++)
+        {
+            heightMaps[i] = null;
+        }
+
+        return (seeds, heightMaps);
+    }
+
+    private static List<(int, float)> FindSeeds(int[] seeds, HeightMap[] heightMaps, Vector3[] spawns)
     {
         const float MAX_DISTANCE_TO_LOG = 3000;
-        List<(int, float)> seeds = new();
+        List<(int, float)> godDistanceSeeds = new();
 
-        // currentSeed >= startSeed is for handling the overflow edge case
-        for (currentSeed = startSeed; currentSeed >= startSeed && currentSeed < endSeed; currentSeed = seedCalculator.CalculateNextGodSeed())
+        for (int i = 0; i < NUM_SEEDS_PER_FRAME && heightMaps[i] != null; i++)
         {
-            HeightMap heightMap = new(currentSeed);
+            List<Vector3> chiefsChests = ChiefsChests.FindChiefsChests(seeds[i], heightMaps[i], out List<Vector3> villages);
+            List<Vector3> guardians = Guardians.FindGuardians(seeds[i], heightMaps[i]);
+            Vector3 boat = Boat.FindBoat(seeds[i], heightMaps[i]);
 
-            Vector3 spawn = Spawn.FindSurvivalSpawn(currentSeed, heightMap);
-            List<Vector3> chiefsChests = ChiefsChests.FindChiefsChests(currentSeed, heightMap, out List<Vector3> villages);
-            List<Vector3> guardians = Guardians.FindGuardians(currentSeed, heightMap);
-            Vector3 boat = Boat.FindBoat(currentSeed, heightMap);
-
-            float distance = CalculateDistance.CalculateTotalDistance(spawn, chiefsChests, guardians, villages, boat);
+            float distance = CalculateDistance.CalculateTotalDistance(spawns[i], chiefsChests, guardians, villages, boat);
             if (distance <= MAX_DISTANCE_TO_LOG)
             {
-                seeds.Add((currentSeed, distance));
-                Debug.Log($"Found seed {currentSeed}, distance {distance}");
+                godDistanceSeeds.Add((seeds[i], distance));
+                //Debug.Log($"Found seed {seeds[i]}, distance {distance}");
             }
         }
 
-        return seeds;
+        return godDistanceSeeds;
     }
 
     private List<(int, float)> FindSeeds()
     {
-        const int SEEDS_PER_THREAD = 10000;
-        List<(int, float)> seeds = new();
-        var tasks = new Task<List<(int, float)>>[NUM_THREADS];
+        List<(int, float)> godDistanceSeeds = new();
+        var heightMapTasks = new Task<(int[], HeightMap[])>[NUM_THREADS];
 
         for (int i = 0; i < NUM_THREADS; i++)
         {
-            int threadEndSeed = currentSeeds[i] + SEEDS_PER_THREAD;
-            if (threadEndSeed > endSeeds[i] || threadEndSeed < currentSeeds[i])
-            {
-                threadEndSeed = endSeeds[i];
-            }
             int index = i; // avoid access to modified closure
-            tasks[i] = Task.Run(() => FindSeeds(seedCalculators[index], currentSeeds[index], threadEndSeed, out currentSeeds[index]));
+            heightMapTasks[i] = Task.Run(() => GetHeightMaps(seedCalculators[index], startSeeds[index], endSeeds[index]));
         }
 
-        foreach (var task in tasks)
+        var findSeedTasks = new Task<List<(int, float)>>[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++)
         {
-            seeds.AddRange(task.Result);
+            var spawns = new Vector3[NUM_SEEDS_PER_FRAME];
+            var (seeds, heightMaps) = heightMapTasks[i].Result;
+            for (int j = 0; j < NUM_SEEDS_PER_FRAME; j++)
+            {
+                if (seeds[j] == -2147320059
+                    || seeds[j] == -2147259148
+                    || seeds[j] == -2147115284
+                    || seeds[j] == -2147001245
+                    || seeds[j] == -2146970159
+                    || seeds[j] == -2146939073
+                    || seeds[j] == -2146826333
+                    || seeds[j] == -2146725197
+                    || seeds[j] == -2146664324
+                    || seeds[j] == -2146652682
+                    || seeds[j] == -2146569729
+                    || seeds[j] == -2146530841
+                    || seeds[j] == -2146499736
+                    )
+                {
+                    Debug.Log(seeds[j]);
+                }
+                if (heightMaps[j] == null)
+                {
+                    break;
+                }
+                spawns[j] = Spawn.FindSurvivalSpawn(seeds[j], heightMaps[j]);
+                if (seeds[j] == -2147320059
+                    || seeds[j] == -2147259148
+                    || seeds[j] == -2147115284
+                    || seeds[j] == -2147001245
+                    || seeds[j] == -2146970159
+                    || seeds[j] == -2146939073
+                    || seeds[j] == -2146826333
+                    || seeds[j] == -2146725197
+                    || seeds[j] == -2146664324
+                    || seeds[j] == -2146652682
+                    || seeds[j] == -2146569729
+                    || seeds[j] == -2146530841
+                    || seeds[j] == -2146499736
+                    )
+                {
+                    Debug.Log(seeds[j]);
+                }
+            }
+
+            findSeedTasks[i] = Task.Run(() => FindSeeds(seeds, heightMaps, spawns));
         }
 
-        return seeds;
+        foreach (var findSeedTask in findSeedTasks)
+        {
+            godDistanceSeeds.AddRange(findSeedTask.Result);
+        }
+
+        return godDistanceSeeds;
     }
 
     private void UpdateText()
@@ -76,7 +138,7 @@ public class MainClass : MonoBehaviour
         int numTestedSeeds = 0;
         for (int i = 0; i < NUM_THREADS; i++)
         {
-            numTestedSeeds += currentSeeds[i] - 1 - startSeeds[i];
+            numTestedSeeds += seedCalculators[i].currentSeed - 1 - startSeeds[i];
         }
 
         seedsTestedText.text = $"Seeds Tested:\n{numTestedSeeds} / {uint.MaxValue}";
@@ -111,7 +173,6 @@ public class MainClass : MonoBehaviour
 
         for (int i = 0; i < NUM_THREADS; i++)
         {
-            currentSeeds[i] = startSeeds[i];
             seedCalculators[i] = new SeedCalculator(startSeeds[i]);
         }
     }
